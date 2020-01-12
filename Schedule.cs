a2727aebab5b245c271c783a082baffedfd0bf36
John Michael <john.michael.nicholson@Gmail.com>
2020-01-11 23:37:19 -0600

    Add files via upload

---
+++ Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingProgram
+{
+    public class Schedule
+    {
+        public string Name {get; set;}
+        public int MondayHours {get; set;}
+        public int TuesdayHours {get; set;}
+        public int WednesdayHours {get; set;}
+    }
+}
