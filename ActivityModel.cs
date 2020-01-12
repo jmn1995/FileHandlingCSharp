6372ad6da42c0491882d470bdb6297a182761dd1
John Michael <john.michael.nicholson@Gmail.com>
2020-01-11 23:36:48 -0600

    Add files via upload

---
+++ ActivityModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingProgram
+{
+   public class ActivityModel
+    {
+        public string ActivityName { get; set; }
+        public string Monday { get; set; }
+        public string Tuesday { get; set; }
+        public string Wednesday { get; set; }
+
+
+
+
+    }
+}
